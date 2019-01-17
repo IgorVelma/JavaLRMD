d17d7f07bf7ba02b9162b97aaebb1b3559f2f65e
IgorVelma <igorknyaz28@gmail.com>
2019-01-17 16:42:15 +0300

    addTests

---
+++ Ex2/test/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void add() {
+        Calculator calculator = new Calculator();
+        calculator.add(1.0d, 1.0d);
+        assertEquals(2.0, calculator.getResult(), 0.1);
+    }
+    @Test
+    public void pow(){
+        Calculator c = new Calculator();
+        c.pow(2, 2);
+        assertEquals(4, c.getResult(), 0.1);
+    }
+}
